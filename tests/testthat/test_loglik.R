bdbfbd1a87483e0968ec9018862acc60aa372fa8
ahboualam <ahmed.boualam@inrae.fr>
2026-02-19 15:27:28 +0100

    Update

--- tests/testthat/test_loglik.R
+++ tests/testthat/test_loglik.R
@@ -7,24 +7,21 @@ data("coordinates", package = "MSTWeatherGen")
 names = c("Precipitation", "Wind", "Temp_max")
 dates = seq(as.Date("2018-01-01"),as.Date("2021-12-31"), by="day")
 names = c("Precipitation", "Wind", "Temp_max")
-
 seasons <- list(
   s1 = list(min_day = 1, max_day = 29, min_month = 12, max_month = 2),
   s2 = list(min_day = 1, max_day = 31, min_month = 3, max_month = 5),
   s3 = list(min_day = 1, max_day = 31, min_month = 6, max_month = 8),
   s4 = list(min_day = 1, max_day = 30, min_month = 9, max_month = 11)
 )
-s1 = list(min_day = 1, max_day = 29, min_month = 12, max_month = 2)
 
+# Parameters:
 par_all <- readRDS(testthat::test_path("saved_results/par_all.rds"))
 Vi <- readRDS(testthat::test_path("saved_results/Vi.rds"))
 uh <- readRDS(testthat::test_path("saved_results/uh.rds"))
 uh <- uh[uh[,1]==0,]
-
 cr <- readRDS(testthat::test_path("saved_results/cr.rds"))
 ep <- readRDS(testthat::test_path("saved_results/ep.rds"))
 max_it <- 50
-
 parms <- c("a1", "d1", "g1", "a2", "d2", "g2",
            "b1", "e1", "l1", "b2", "e2", "l2", "c", "f", "m",
            paste(names, "ai", sep = ":"), paste(names, "bi", sep = ":"),
@@ -33,9 +30,7 @@ par <- par_all[parms]
 u <- NULL
 h <- NULL
 
-skip_on_cran()
-skip_on_ci()
-
+# -------------------------------------------------Tests -----------------------------------------------------------
 result <- loglik(par = par, parms = parms, par_all = par_all,
                  data = data, names = names, Vi = Vi,
                  h = h, u = u, uh = uh, ep = ep, cr = cr)
