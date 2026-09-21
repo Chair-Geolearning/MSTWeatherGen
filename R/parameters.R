74180b5dc609f3259d72841329b43af65af803b3
ahboualam <ahmed.boualam@inrae.fr>
2026-09-21 16:48:50 +0200

    initialistion params

--- R/parameters.R
+++ R/parameters.R
@@ -95,7 +95,7 @@
   1,     # ajj (depuis par_s)
   1,   # nuii (depuis par_s)
   1,   # nujj (depuis par_s)
-  0.9, # rho1ij self-pair
+  0.5, # rho1ij self-pair
   1,     # r2ii
   1,     # r2jj
   1,     # r1ii
